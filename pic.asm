71427db84b42acf34ce8b4c7ba78eeea50b9aabe
John R Dietrick <john@johndietrick.com>
2016-04-30 17:37:59 +0700

    Stub in code for masking all IRQs

--- pic.asm
+++ pic.asm
@@ -9,6 +9,11 @@ align 16, db 0
 %define SLAVE_PIC_COMMAND 0xa0
 %define SLAVE_PIC_DATA (SLAVE_PIC_COMMAND + 1)
 
+disable_irq:
+    ; TODO
+    nop
+    ret
+
 initialize_8259:
     mov dx, MASTER_PIC_COMMAND
     mov ax, 0x11
@@ -30,4 +35,15 @@ initialize_8259:
     out dx, ax
     mov ax, 0x01
     out dx, ax
+
+    ; Disable all IRQs
+    mov ecx, 0
+.loop:
+    push ecx
+    call disable_irq
+    pop ecx
+    inc ecx
+    cmp ecx, 16
+    jl .loop
+
     ret
