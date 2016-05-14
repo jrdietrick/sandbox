2cd8bd272fb8dd8d31a906b9769dc54e63f0a605
John R Dietrick <john@johndietrick.com>
2016-05-15 01:16:34 +0800

    Scancode table

--- keyboard.asm
+++ keyboard.asm
@@ -9,6 +9,9 @@ align 16, db 0
 
 %define KEYBOARD_DATA_PORT 0x60
 
+%define NON_PRINTABLE_CHARACTER 0xff
+%define NPC NON_PRINTABLE_CHARACTER
+
 keyboard_interrupt_handler:
     pushad
 
@@ -24,3 +27,39 @@ keyboard_interrupt_handler:
     add esp, 4
     popad
     iret
+
+align 16, db 0
+
+scancode_table:
+    db NPC, NPC, '1', '2', '3', '4', '5', '6'
+    db '7', '8', '9', '0', '-', '=', NPC, NPC
+    db 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i'
+    db 'o', 'p', '[', ']', 0xa, NPC, 'a', 's'
+    db 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'
+    db 0x27, '`', NPC, '\', 'z', 'x', 'c', 'v'
+    db 'b', 'n', 'm', ',', '.', '/', NPC, NPC
+    db NPC, ' ', NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, '!', '@', '#', '$', '%', '^'
+    db '&', '*', '(', ')', '_', '+', NPC, NPC
+    db 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I'
+    db 'O', 'P', '[', ']', 0xa, NPC, 'A', 'S'
+    db 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':'
+    db '"', '~', NPC, '|', 'Z', 'X', 'C', 'V'
+    db 'B', 'N', 'M', '<', '>', '?', NPC, NPC
+    db NPC, ' ', NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
+    db NPC, NPC, NPC, NPC, NPC, NPC, NPC, NPC
