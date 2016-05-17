405e9a9bddf8972d06c7f640a56a41a6b963c5b1
John R Dietrick <john@johndietrick.com>
2016-05-17 14:04:06 +0800

    Arithmetic error in filesystem image assembly

--- usermode_programs/assemble_fs_image.sh
+++ usermode_programs/assemble_fs_image.sh
@@ -4,7 +4,7 @@ shift
 dd if=/dev/zero bs=512 count=32 status=none > $output_file
 offset=0
 for file in $@; do
-    dd if=$file of=$output_file bs=512 seek=$offset count=8 conv=notrunc status=none
+    dd if=$file of=$output_file bs=512 seek=$((offset*8)) count=8 conv=notrunc status=none
     offset=$((offset+1))
     if ((offset >= 4)); then
         break
